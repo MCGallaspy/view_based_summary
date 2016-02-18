cdc98630002388ccf04277f39cd80828a5b8ab69
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-17 19:39:28 -0800

    Stub out view

--- view_based_summary/view_based_summary/urls.py
+++ view_based_summary/view_based_summary/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from views import SummaryView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^summary/', SummaryView.as_view()),
 ]
