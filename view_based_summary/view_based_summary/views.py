b0a790a00bc212ce6cbe17e60563c682e5120775
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-17 19:58:05 -0800

    Create basic uncached view

--- view_based_summary/view_based_summary/views.py
+++ view_based_summary/view_based_summary/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import View
 from django.http import HttpResponse
 
+from models import ContentInteractionLog
+
 
 class SummaryView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("foo")
+        user = kwargs['user']
+        total = 0
+
+        for cl in ContentInteractionLog.objects.filter(user=user):
+            total += cl.completion_timestamp - cl.start_timestamp
+
+        return HttpResponse(str(total))
