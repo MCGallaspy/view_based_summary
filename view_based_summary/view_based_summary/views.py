e10b19967a86a910db9c4bcd5410ce0be7bfa9d4
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-02-17 20:01:49 -0800

    Cache view result

--- view_based_summary/view_based_summary/views.py
+++ view_based_summary/view_based_summary/views.py
@@ -3,13 +3,21 @@ from django.http import HttpResponse
 
 from models import ContentInteractionLog
 
+from functools32 import lru_cache
+
 
 class SummaryView(View):
     def get(self, request, *args, **kwargs):
         user = kwargs['user']
-        total = 0
-
-        for cl in ContentInteractionLog.objects.filter(user=user):
-            total += cl.completion_timestamp - cl.start_timestamp
-
+        total = summary_view_helper(user)
         return HttpResponse(str(total))
+
+
+@lru_cache(maxsize=128)
+def summary_view_helper(user):
+    total = 0
+
+    for cl in ContentInteractionLog.objects.filter(user=user):
+        total += cl.completion_timestamp - cl.start_timestamp
+
+    return total
